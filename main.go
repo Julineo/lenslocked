0674e391155319263b8c8ed95d78bc134a5e4087
Julineo <advoretskiy80@gmail.com>
2021-04-26 22:56:11 -0700

    added  custom 404

--- main.go
+++ main.go
@@ -1,25 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html")
-  fmt.Fprint(w, "Send email to <a href=\"mailto:support@lenslocked.com\">support@lensllocked.com</a>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Send email to <a href=\"mailto:support@lenslocked.com\">support@lensllocked.com</a>")
+}
+
+func custom404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "custom 404 not found")
 }
 
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/", home)
-  r.HandleFunc("/contact", contact)
-  http.ListenAndServe(":3000", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(custom404)
+	http.ListenAndServe(":3000", r)
 }
