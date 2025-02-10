0e810ec7137e1e87fdae67ea645a73d2e88ff130
Alex Dvoretskiy <advoretskiy80@gmail.com>
2025-02-10 14:09:17 -0800

    added page not found handler

--- main.go
+++ main.go
@@ -15,9 +15,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Contact Page</h1><p>Contact us at <a href=\"mailto:info@gmail.com\">infogmail.com</a></p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFoundHandler().ServeHTTP(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
